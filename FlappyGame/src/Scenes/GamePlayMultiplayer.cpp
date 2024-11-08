bcc4340eb10472d7f57b3888f7961ec7e3215b03
Alex Gallardo <gallardonicolas.8100@outlook.com>
2024-11-08 20:56:06 -0300

    implement a game Multiplayer by copying it and reimplemented a second player in it

---
+++ FlappyGame/src/Scenes/GamePlayMultiplayer.cpp
@@ -0,0 +1,241 @@
+#include "GamePlay.h"
+
+#include "GameLoop.h"
+
+#include "Entities/obstacle.h"
+#include "Entities/Player.h"
+
+#include "UI/Button.h"
+
+#include "Utilities/Utils.h"
+
+namespace game
+{
+	namespace scenes
+	{
+		namespace gameplayMultiplayer
+		{
+			player::Player pl;
+			player::Player pl2;
+			obstacle::Obstacle obs;
+
+			button::Button resume;
+			button::Button menu;
+
+			Texture2D backGround;
+			Texture2D midGround;
+			Texture2D foreGround;
+
+			float offsetBack;
+			float offsetMid;
+			float offsetFore;
+
+			float velBack;
+			float velMid;
+			float velFore;
+
+			constexpr float TimerStart = 3.0f;
+			float Timer;
+
+			bool pause;
+
+			void InitEntities();
+
+			void ParalaxUpdate();
+
+
+
+			void Init() {
+				backGround = LoadTexture("res/BackGround.png");
+				midGround = LoadTexture("res/MidGround.png");
+				foreGround = LoadTexture("res/ForeGround.png");
+
+				backGround.width *= GetScreenWidth() / backGround.width;
+				backGround.height *= GetScreenHeight() / backGround.height;
+
+				midGround.width *= GetScreenWidth() / midGround.width;
+				midGround.height *= GetScreenHeight() / midGround.height;
+
+				foreGround.width *= GetScreenWidth() / foreGround.width;
+				foreGround.height *= GetScreenHeight() / foreGround.height;
+
+				offsetBack = 0.0;
+				offsetMid = 0.0;
+				offsetFore = 0.0;
+
+				velBack = 25.f;
+				velMid = 100.f;
+				velFore = 250.f;
+
+				pause = false;
+
+				Rectangle posButton;
+
+				posButton.x = static_cast<float>(screenWidth) / 2.0f;
+				posButton.y = static_cast<float>(screenHeight) / 2.0f - button::ButtonHeight * 1.2f;
+				posButton.width = button::ButtonWidth;
+				posButton.height = button::ButtonHeight;
+
+				resume = button::Create(posButton, "Resume");
+
+				posButton.y = static_cast<float>(screenHeight) / 2.0f + button::ButtonHeight * 2.4f;
+
+				menu = button::Create(posButton, "Menu");
+
+				InitEntities();
+			}
+
+
+
+			void Input() {
+				if (Timer > TimerStart) {
+					if (IsKeyPressed(KEY_ESCAPE))
+						pause = !pause;
+
+					if (pause) {
+						if (button::IsPressed(resume))
+							pause = !pause;
+
+						if (button::IsPressed(menu)) {
+							currentScene = Scene::Menu;
+							pause = !pause;
+							InitEntities();
+						}
+
+						return;
+					}
+
+					player::Input(pl, KEY_W);
+					player::Input(pl2, KEY_UP);
+				}
+			}
+
+
+
+			void Update() {
+				Timer += GetFrameTime();
+				if (Timer > TimerStart) {
+					if (pause) {
+						button::MouseOnTop(resume);
+						button::MouseOnTop(menu);
+
+						return;
+					}
+
+					ParalaxUpdate();
+
+					player::Update(pl);
+					player::Update(pl2);
+					obstacle::Update(obs);
+
+					if (obs.body1.x + obs.body1.width < 0) {
+						Vector2 newPos;
+
+						newPos.x = static_cast<float>(GetScreenWidth());
+						newPos.y = static_cast<float>(
+							GetRandomValue(0, GetScreenHeight() - static_cast<int>(obs.body1.height)));
+
+						obstacle::SetPosition(obs, newPos);
+					}
+
+					if (CheckCollision(pl.body, obs.body1) || CheckCollision(pl.body, obs.body2) || CheckCollision(
+						    pl2.body, obs.body1) || CheckCollision(pl2.body, obs.body2)) {
+						InitEntities();
+						currentScene = Scene::Menu;
+					}
+
+					if (CheckBorderCollision(pl.body, GetScreenWidth(), 0, GetScreenHeight(), 0) || CheckBorderCollision(
+						    pl2.body, GetScreenWidth(), 0, GetScreenHeight(), 0)) {
+						if (pl.body.y < 0)
+							pl.body.y = 0;
+						if (pl.body.y + pl.body.height > static_cast<float>(GetScreenHeight())) {
+							InitEntities();
+							currentScene = Scene::Menu;
+						}
+						if (pl2.body.y < 0)
+							pl2.body.y = 0;
+						if (pl2.body.y + pl2.body.height > static_cast<float>(GetScreenHeight())) {
+							InitEntities();
+							currentScene = Scene::Menu;
+						}
+					}
+				}
+			}
+
+
+
+			void Draw() {
+				Color transparent = { 255, 255, 255, 200 };
+
+				DrawTextureEx(backGround, { offsetBack, 0 }, 0, 1, WHITE);
+				DrawTextureEx(backGround, { static_cast<float>(backGround.width) + offsetBack, 0 }, 0, 1, WHITE);
+
+				DrawTextureEx(midGround, { offsetMid, 0 }, 0, 1, transparent);
+				DrawTextureEx(midGround, { static_cast<float>(midGround.width) + offsetMid, 0 }, 0, 1, transparent);
+
+				player::Draw(pl);
+				player::Draw(pl2);
+				obstacle::Draw(obs);
+
+				DrawTextureEx(foreGround, { offsetFore, 0 }, 0, 1, WHITE);
+				DrawTextureEx(foreGround, { static_cast<float>(foreGround.width) + offsetFore, 0 }, 0, 1, WHITE);
+
+				DrawRect(Rectangle{ 0,
+				                    static_cast<float>(screenHeight) / 5.0f,
+				                    static_cast<float>(screenWidth),
+				                    static_cast<float>(screenHeight) }, Color{ 37, 107, 122, 150 });
+
+				if (pause) {
+					DrawRect(Rectangle{ 0, 0, static_cast<float>(screenWidth), static_cast<float>(screenHeight) },
+					         Color{ 50, 50, 50, 200 });
+
+					button::Draw(resume);
+					button::Draw(menu);
+				}
+
+				if (Timer < TimerStart)
+				{
+					DrawRectangle(0,0, GetScreenWidth(), GetScreenWidth(), {0, 0, 0, 100} );
+					DrawText("Player 1 jumps with W", 20, 100, 48, WHITE);
+					DrawText("Player 2 jumps with Up key", 20, 160, 48, WHITE);
+					DrawText(TextFormat("%0.1f", (TimerStart - Timer)), GetScreenWidth() / 2, 215, 48, WHITE);
+				}
+			}
+
+
+
+			void DeInit() {
+				UnloadTexture(backGround);
+				UnloadTexture(midGround);
+				UnloadTexture(foreGround);
+
+				player::DeInit(pl);
+				player::DeInit(pl2);
+			}
+
+
+
+			void InitEntities() {
+				float randomY = static_cast<float>(GetRandomValue(200, GetScreenHeight() - static_cast<int>(obs.body1.height / 2)));
+
+				float x = static_cast<float>(GetScreenWidth()) / 4.0f;
+				float y = static_cast<float>(GetScreenHeight()) / 2.0f;
+
+				pl = player::Create(Rectangle{ x, y, 60, 60 }, 300.f);
+				pl2 = player::Create(Rectangle{ x, y, 60, 60 }, 300.f);
+
+				x = static_cast<float>(GetScreenWidth() + 20);
+				obs = obstacle::Create(x, randomY, 40, 1000, 500.f);
+			}
+
+
+
+			void ParalaxUpdate() {
+				offsetBack -= (offsetBack <= static_cast<float>(-backGround.width)) ? offsetBack : velBack * GetFrameTime();
+				offsetMid -= (offsetMid <= static_cast<float>(-midGround.width)) ? offsetMid : velMid * GetFrameTime();
+				offsetFore -= (offsetFore <= static_cast<float>(-foreGround.width)) ? offsetFore : velFore * GetFrameTime();
+			}
+
+		}
+	}
+}
---
+++ FlappyGame/src/Scenes/GamePlayMultiplayer.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace game
+{
+	namespace scenes
+	{
+		namespace gameplayMultiplayer
+		{
+			void Init();
+			void Input();
+			void Update();
+			void Draw();
+
+			void DeInit();
+		}
+	}
+}
