8db0c9fd28992baabd169d557feee32d932bc659
Stevendotcom <gallardonicolas.8100@outlook.com>
2024-11-15 12:30:59 -0300

    fix issue with .bat

--- FlappyGame/scripts/copy_files.bat
+++ FlappyGame/scripts/copy_files.bat
@@ -3,7 +3,7 @@
 echo %1 Solution Dir
 echo %2 Target Dir
 
-del "%~1bin\FlappyGame.pdb"
+if %3==1 del "%~1bin\FlappyGame.pdb"
 
 xcopy "%~1res" "%~2res" /i /y /s
 tar --create --file=FlappyGame_.zip -C "%~1bin" .
